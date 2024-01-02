63de19f4b15560364409cd2520da65c5f41aa53f
Karan <karanpraja902@gmail.com>
2024-01-02 13:35:27 +0530

    added homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import  ProductList  from "../features/counter/Counter"
+import Navbar from "../features/navbar/Navbar"
+
+const Home=()=>{
+return(
+    <Navbar>
+        <ProductList></ProductList>
+    </Navbar>
+)
+}
+export default Home
